49831e34da11ae0b5fca783c371fd7a9b9f95db2
Gus <g.sosa1@lab.matcom.uh.cu>
2016-10-06 18:42:22 -0500

    2698 Problem resolved

---
+++ 2694.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Gus on 10/6/2016.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "This is my first solution in programming competitions";
+    return 0;
+}
+
