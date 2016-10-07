f5e056c98dd04d0367a7d60cc9d5519cc5b76da7
Gus <g.sosa1@lab.matcom.uh.cu>
2016-10-06 19:02:31 -0500

    ....

---
+++ 2769.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Gus on 10/6/2016.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        cout << i << ": I am participating in the Engineer's day." << endl;
+    }
+
+    return 0;
+}
